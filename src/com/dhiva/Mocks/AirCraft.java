ce6fbc507d745366fb04eb07f61befede07e41a2
Dhivashini Jaganathan <dhivashini.jaganathan@sjsu.edu>
2017-01-31 19:35:47 -0800

    updated position, enum and time complexity for methods

--- src/com/dhiva/Mocks/AirCraft.java
+++ src/com/dhiva/Mocks/AirCraft.java
@@ -1,19 +1,45 @@
 package com.dhiva.Mocks;
 
 public class AirCraft {
-	 int id;
-	 String carrierName;
-	String type;
-	 String size;
+	int id;
+	String carrierName;
+	Type type;
+	Size size;
 
-	AirCraft(int id, String carrierName, String type, String size) {
+	public enum Type {
+		PASSENGER("PASSENGER"), CARGO("CARGO");
+		String type;
+
+		Type(String type) {
+			this.type = type;
+		}
+	}
+
+	public enum Size {
+		LARGE("LARGE"), SMALL("SMALL");
+		String size;
+
+		Size(String size) {
+			this.size = size;
+		}
+	}
+
+	AirCraft(int id, String carrierName, Type type, Size size) {
 		this.id = id;
 		this.carrierName = carrierName;
 		this.type = type;
 		this.size = size;
 	}
-	
-	public String getName(){
+
+	public String getType() {
+		return this.type.toString();
+	}
+
+	public String getSize() {
+		return this.size.toString();
+	}
+
+	public String getName() {
 		return this.carrierName;
 	}
 }
--- src/com/dhiva/Mocks/AirCraftFunctions.java
+++ src/com/dhiva/Mocks/AirCraftFunctions.java
@@ -10,14 +10,13 @@ public class AirCraftFunctions {
 	static PriorityQueue<AirCraft> acQueue = new PriorityQueue<AirCraft>(10, new Comparator<AirCraft>() {
 		@Override
 		public int compare(AirCraft o1, AirCraft o2) {
-			if (o1.type.toString().compareTo(o2.type.toString()) > 0) {
+			if (o1.getType() == "PASSENGER") {
 				return 1;
-			}
-			if (o1.type.toString().compareTo(o2.type.toString()) == 0) {
-				if (o1.size.toString() == "Large") {
+			} else if (o1.type == o2.type) {
+				if (o1.getSize() == "LARGE") {
 					return 1;
 				}
-				if (o1.size.toString().compareTo(o2.type.toString()) == 0)
+				if (o1.size == o2.size)
 					return 0;
 				return -1;
 			}
@@ -27,18 +26,23 @@ public class AirCraftFunctions {
 
 	static HashMap<AirCraft, Integer> positionMap;
 
-	// o(1) time.
+	// O(log(n)) time.
 	public static void enqueue(AirCraft ac) {
 		acQueue.add(ac);
 		position++;
 		positionMap.put(ac, position);
 	}
 
-	// o(1) time. just removing the head of the queue.
+	// o(n) time. To update the position value in positionMap.
 	public static AirCraft dequeue(AirCraft ac) {
 		if (!acQueue.isEmpty()) {
 			AirCraft obj = acQueue.poll();
 			positionMap.remove(obj);
+			Iterator<Integer> i = positionMap.values().iterator();
+			while (i.hasNext()) {
+				int pos = i.next();
+				pos--;
+			}
 			return obj;
 		}
 		return null;
